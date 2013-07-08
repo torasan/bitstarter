e9fcbd3c7aa0ca1bd7a49321309f49fade975ffc
Tora San <battambang@gmail.com>
2013-07-08 17:45:28 +1000

    Modify web.js to include index.html

--- web.js
+++ web.js
@@ -2,8 +2,18 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var sendData;
+
+fs = require('fs')
+fs.readFile('./index.html', 'utf8', function (err,data) {
+  if (err) throw err;
+  sendData = data;
+  //console.log(data);
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  response.send(sendData);
 });
 
 var port = process.env.PORT || 5000;
